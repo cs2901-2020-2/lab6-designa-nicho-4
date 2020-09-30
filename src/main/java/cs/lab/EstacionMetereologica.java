93fd5aa062c24d5a87c11d5579b17d457f296cef
THEFLILUX <jorge.nicho@utec.edu.pe>
2020-09-30 16:01:13 -0500

    avance

--- src/main/java/cs/lab/EstacionMetereologica.java
+++ src/main/java/cs/lab/EstacionMetereologica.java
@@ -5,55 +5,69 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class EstacionMetereologica{
+    static final Logger logger = Logger.getLogger(EstacionMetereologica.class.getName());
 
-    public class Cliente{
+    double humedad;
+    double temperatura;
+    double viento;
+
+    public EstacionMetereologica(){}
+
+    public double getHumedad() {
+        return humedad;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+    public double getViento() {
+        return viento;
+    }
+
+    public static class Cliente{
 
         public double humedad;
         public double temperatura;
         public double viento;
         public String username;
-    
+
         final Logger logger = Logger.getLogger(Cliente.class.getName());
-    
+
         public Cliente(String _username) {
             username  = _username;
             logger.info("Starting analysis...");
         }
-    
+
         public Cliente(){}
-        
+
         public void update(double _temperatura, double _humedad, double _viento){
             temperatura = _temperatura;
             humedad = _humedad;
             viento = _viento;
         }
-    
+
     }
 
-    static final Logger logger = Logger.getLogger(EstacionMetereologica.class.getName());
-
-    List<Cliente> clientes = new ArrayList<Cliente>();
-
-    public EstacionMetereologica(){}
+    List<Cliente> clientes = new ArrayList<>();
 
     public void addUser(Cliente cliente){
         clientes.add(cliente);
     }
 
-    public void removeUser(Cliente user){
-        for(int i = 0; i < clientes.size(); i++){
-            if(clientes.get(i).username == user.username){
-                clientes.remove(i);
-            }
+    public void update(double temperatura, double humedad, double viento){
+        for(Cliente i: clientes){
+            i.update(temperatura, humedad, viento);
         }
     }
 
-    public void update(String filename){
-        
-    }
-
     public static void main(String[] args) {
-        
+        Cliente cliente = new Cliente();
+
+        cliente.username = "marlon";
+
+        EstacionMetereologica clima = new EstacionMetereologica();
+        clima.update(10, 23, 100);
+
     }
 
 }
