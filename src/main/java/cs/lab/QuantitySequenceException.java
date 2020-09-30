6a53056b15ed48c6317ce216073a8f61bae286be
Alejandro  Otero <alejandrotero@Alejandros-MacBook-Air.local>
2020-09-30 10:58:05 -0500

    Code uploaded

--- src/main/java/cs/lab/QuantitySequenceException.java
+++ src/main/java/cs/lab/QuantitySequenceException.java
@@ -1,8 +1,33 @@
 package cs.lab;
 
-public class QuantitySequenceException extends Exception {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
-    public QuantitySequenceException(String message){
-        super(message);
+public class EstacionMetereologica{
+
+    static final Logger logger = Logger.getLogger(Cliente.class.getName());
+
+    List<Cliente> clientes = new ArrayList<Cliente>();
+
+    public EstacionMetereologica(){
+
     }
+
+    public void addUser(Cliente cliente){
+        clientes.add(cliente);
+    }
+
+    public void removeUser(Cliente user){
+        for(int i = 0; i < clientes.size(); i++){
+            if(clientes[i].username == user.username){
+                clientes.remove(i);
+            }
+        }
+    }
+
+    public void update(String filename){
+        
+    }
+
 }
--- src/main/java/cs/lab/SequenceSizeException.java
+++
@@ -1,8 +0,0 @@
-package cs.lab;
-
-public class SequenceSizeException extends Exception {
-
-    public SequenceSizeException(String message){
-        super(message);
-    }
-}
